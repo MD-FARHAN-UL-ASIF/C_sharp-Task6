ce7a240c0393c67e02661b8b2cc9a057e48192df
MD. FARHAN UL ASIF <100662074+MD-FARHAN-UL-ASIF@users.noreply.github.com>
2022-07-25 20:33:43 +0600

    Add files via upload
    [There is a specific format for both Laborer’s and Manager’s id. For Laborer it’s PL-XXX and Manager its PM-XX. The id number needs to be auto generating. For both start the value of id from 1.]This Part is optional
    The Main method is given below for you. Your job is to implement the classes along with the relationships.
    class Program { private static void Main(string[] args) { ServiceProvider.Inset(new Manager("PM-01", "Shuvo", "Uttara", 5)); ServiceProvider.Inset(new Manager("PM-02", "Jahid", "Badda", 1)); ServiceProvider.Inset(new Laborer("PL-001", "Sohan", "Mirpur", 85, 20)); ServiceProvider.Inset(new Laborer("PL-002", "Jisan", "Kuril", 65, 44)); ServiceProvider.Inset(new Laborer("PL-003", "Numan", "Banani", 88, 3)); //Manager(id,name,address,yearsOfExperience) //Laborer(id,name,address,userrating,noOfServices) ServiceProvider.ProvideDetails(); ServiceProvider.SkilledWorker(); } }

---
+++ Employee.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB_Task_7
+{
+    class Employee
+    {
+        public string name;
+        public string id;
+        public string address;
+
+        public Employee(string name, string id, string address)
+        {
+            this.name = name;
+            this.id = id;
+            this.address = address;
+        }
+
+        public void showInfo()
+        {
+            Console.WriteLine("Employee name: " + name);
+            Console.WriteLine("Employee id: " + id);
+            Console.WriteLine("Employee address: " + address);
+        }
+
+
+        public bool EmployeeStatus()
+        {
+            return false;
+        }
+    }
+
+    class Laborer : Employee
+    {
+        public int userRating;
+        public String chargePerService = "500 BDT";
+        public int noOfService;
+
+        public Laborer(String id, String name, String address, int userRating, int noOfService) : base(id, name, address)
+        {
+            this.userRating = userRating;
+            this.noOfService = noOfService;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Laborer name: " + name);
+            Console.WriteLine("Laborer id: " + id);
+            Console.WriteLine("Laborer address: " + address);
+            Console.WriteLine("EmployeeStatus:" + this.EmployeeStatus());
+        }
+
+        public bool EmployeeStatus()
+        {
+            return userRating > 70 && noOfService >= 10;
+        }
+
+        public int TotalEarn()
+        {
+            return 10000;
+        }
+    }
+
+    class Manager : Employee
+    {
+        public int yearOfExperience;
+
+        public Manager(String id, String name, String address, int yearOfExperience) : base(id, name, address)
+        {
+            this.yearOfExperience = yearOfExperience;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Manager name: " + name);
+            Console.WriteLine("Manager id: " + id);
+            Console.WriteLine("Manager address: " + address);
+            Console.WriteLine("EmployeeStatus:" + this.EmployeeStatus());
+        }
+
+        public bool EmployeeStatus()
+        {
+            return yearOfExperience > 2;
+        }
+    }
+
+    class ServiceProvider
+    {
+        public static List<Employee> employees = new List<Employee>();
+        public static void Inset(Employee emp)
+        {
+            employees.Add(emp);
+        }
+
+        public static void ProvideDetails()
+        {
+            foreach (var emp in employees)
+            {
+                emp.showInfo();
+            }
+        }
+
+        public static void SkilledWorker()
+        {
+            foreach (var emp in employees)
+            {
+                if (emp.GetType() == typeof(Laborer))
+                {
+                    Laborer laborer = (Laborer)emp;
+                    if (laborer.userRating > 80)
+                    {
+                        laborer.showInfo();
+                    }
+                }
+            }
+        }
+
+    }
+}
